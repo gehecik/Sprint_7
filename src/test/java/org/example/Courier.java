02a2c284658485620ccb7044a2188c32ffcf7cec
Kechek Karine <kechek.karine@gmail.com>
2026-04-12 10:21:55 +0300

    add gson, add creating courier

--- src/test/java/org/example/Courier.java
+++ src/test/java/org/example/Courier.java
@@ -4,10 +4,12 @@ public class Courier {
 
     private String login;
     private String password;
+    private String firstName;
 
-    public Courier(String login, String password) {
+    public Courier(String login, String password, String firstName) {
         this.login = login;
         this.password = password;
+        this.firstName = firstName;
     }
 
     public Courier() {
@@ -29,4 +31,11 @@ public class Courier {
         this.password = password;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
--- src/test/java/org/example/CreateCourierTest.java
+++ src/test/java/org/example/CreateCourierTest.java
@@ -6,13 +6,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static io.restassured.RestAssured.given;
 import static org.example.EnvConfig.BASE_URL;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class CreateCourierTest {
 
-    //Courier courier = new Courier("ninja", "1234");
-    String body = "{ \"login\": \"ninja\", \"password\": \"1234\" }";
+    Random random = new Random();
+    int randomNum = random.nextInt();
+
+    Courier courier = new Courier(String.format("user%d", randomNum), "1234", "firstname");
 
     @BeforeEach
     public void setUp() {
@@ -20,14 +25,71 @@ public class CreateCourierTest {
     }
 
     @Test
-    @DisplayName("Test")
-    @Description("200: Test")
-    void test() {
+    @DisplayName("Successful account creation")
+    @Description("201: Successful account creation")
+    void CreateCourierCode201Test() {
+        String body = "{ \"login\": \"loginname\"," +
+                " \"password\": \"1234\", " +
+                "\"firstName\": \"loginfirstname\" }";
+        given()
+                .header("Content-type", "application/json")
+                .body(courier)
+                .when()
+                .post("/api/v1/courier")
+                .then().statusCode(201)
+                .body("ok", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Creating an account without a login")
+    @Description("400: Creating an account without a login")
+    void CreateCourierCode400WithoutLoginTest() {
+        String body = "{ \"password\": \"1234\", " +
+                "\"firstName\": \"loginfirstname\" }";
         given()
                 .header("Content-type", "application/json")
                 .body(body)
                 .when()
-                .post("/api/v1/courier/login")
-                .then().statusCode(200);
+                .post("/api/v1/courier")
+                .then().statusCode(400)
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
+
+    @Test
+    @DisplayName("Creating an account without a password")
+    @Description("400: Creating an account without a password")
+    void CreateCourierCode400WithoutPasswordTest() {
+        String body = "{ \"login\": \"loginname\"," +
+                "\"firstName\": \"loginfirstname\" }";
+        given()
+                .header("Content-type", "application/json")
+                .body(body)
+                .when()
+                .post("/api/v1/courier")
+                .then().statusCode(400)
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
+
+    @Test
+    @DisplayName("Creating an account with a duplicate login")
+    @Description("409: Creating an account with a duplicate login")
+    void CreateCourierCode409WithDuplicateLoginTest() {
+        String body = "{ \"login\": \"loginname1\"," +
+                " \"password\": \"1234\", " +
+                "\"firstName\": \"loginfirstname\" }";
+        given()
+                .header("Content-type", "application/json")
+                .body(body)
+                .post("/api/v1/courier");
+
+        given()
+                .header("Content-type", "application/json")
+                .body(body)
+                .when()
+                .post("/api/v1/courier")
+                .then().statusCode(409)
+                .body("message", equalTo("Этот логин уже используется"));
+        //Expected: Этот логин уже используется
+        //  Actual: Этот логин уже используется. Попробуйте другой.
     }
 }
