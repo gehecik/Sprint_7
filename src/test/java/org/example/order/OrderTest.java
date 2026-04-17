87926e60245e68e0dde08bc017d6d32c3f640508
Kechek Karine <kechek.karine@gmail.com>
2026-04-17 20:35:22 +0300

    add page object for order

--- src/test/java/org/example/order/OrderTest.java
+++ src/test/java/org/example/order/OrderTest.java
@@ -1,4 +1,42 @@
 package org.example.order;
 
-public class OrderTest {
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.example.BaseTest;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderTest extends BaseTest {
+
+    @Step("Create order")
+    public Response createOrder(Object element) {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(element)
+                .post(BASE_PATH + "/orders");
+        return response;
+    }
+
+    @Step("Cancel order")
+    public void cancelOrder(int track) {
+        given()
+                .body(Map.of("track", track))
+                .put(BASE_PATH + "/orders/cancel" );
+    }
+
+    @Step("Get orders list")
+    public Response getOrders(Map<String, Object> parameters) {
+        return given()
+                .queryParams(parameters)
+                .get(BASE_PATH + "/orders");
+    }
+
+//    @Step("Parameter for the request")
+//    public void parameterForOrders(String name, Object value) {
+//        given()
+//
+//    }
 }
